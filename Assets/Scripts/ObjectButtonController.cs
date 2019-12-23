8413644e46bcd43e024ee4e1467cd3f4bcc8cbfd
Mahmut Sat <mmahmutsat@gmail.com>
2019-12-23 16:52:13 +0300

    Project Added to GitHub

---
+++ Assets/Scripts/ObjectButtonController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectButtonController : MonoBehaviour
+{
+    public Button m_PlayButton;
+    void Start()
+    {
+        m_PlayButton.enabled = true;
+        m_PlayButton.GetComponent<Image>().enabled = true;
+    }
+}
